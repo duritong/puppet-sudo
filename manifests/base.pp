16a60525979b2966ee471be092e37e6ac6de35f0
mh <mh@immerda.ch>
2015-11-28 17:41:54 +0100

    linting for future parser

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,11 +1,14 @@
+# basic sudo support
 class sudo::base {
-    if $sudo::deploy_sudoers {
-      file{'/etc/sudoers':
-        source => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
-                    "puppet:///modules/site_sudo/sudoers/sudoers",
-                    "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
-                    "puppet:///modules/sudo/sudoers/sudoers" ],
-        owner => root, group => 0, mode => 0440;
-      }
+  if $sudo::deploy_sudoers {
+    file{'/etc/sudoers':
+      source => [ "puppet:///modules/site_sudo/sudoers/${::fqdn}/sudoers",
+                  'puppet:///modules/site_sudo/sudoers/sudoers',
+                  "puppet:///modules/sudo/sudoers/${::operatingsystem}/sudoers",
+                  'puppet:///modules/sudo/sudoers/sudoers' ],
+      owner  => root,
+      group  => 0,
+      mode   => '0440';
     }
+  }
 }
